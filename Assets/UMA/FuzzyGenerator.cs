6db511c73c43e848241141d94a0dd32f83639e8f
Jefferson Henrique <jeffersonpi@hotmail.com>
2014-06-23 00:18:49 -0300

    - Conversão a partir do Aesthetics

--- Assets/UMA/FuzzyGenerator.cs
+++ Assets/UMA/FuzzyGenerator.cs
@@ -33,26 +33,41 @@ public class FuzzyGenerator : MonoBehaviour {
 
 	private FuzzyQuestionary fuzzyQuestionary;
 
+	public bool fromAesthetic;
+
 	// Update is called once per frame
 	void Start () {
+		if (fromAesthetic) {
+			string[] aux = attributesInput;
+			attributesInput = attributesOutput;
+			attributesOutput = aux;
+		}
+
 		int numAttrs = attributesInput.Length;
 		int numTerms = termLinguistics.Length;
 
 		questionState = new int[numAttrs];
 
 		fuzzyQuestionary = new FuzzyQuestionary();
-		ShowQuestion ();
 	}
 
 	public void ShowQuestion() {
-		string text = "Model a char where:";
+		if (fromAesthetic) {
+			questionLabel.text = "How much in each attribute do you think this character has?";
 
-		for (int index = 0; index < attributesInput.Length; index++) {
-			text += "\n{0} is '{1}'";
-			text = string.Format(text, attributesInput[index], termLinguistics[questionState[index]]);
+			for (int index = 0; index < attributesInput.Length; index++) {
+				adjustChar.UpdateValue(attributesInput[index], 0.5f * questionState[index]);
+			}
+		} else {
+			string text = "Model a char where:";
+
+			for (int index = 0; index < attributesInput.Length; index++) {
+					text += "\n{0} is '{1}'";
+					text = string.Format (text, attributesInput [index], termLinguistics [questionState [index]]);
+			}
+
+			questionLabel.text = text;
 		}
-
-		questionLabel.text = text;
 	}
 
 	public void NextQuestion() {
@@ -91,7 +106,12 @@ public class FuzzyGenerator : MonoBehaviour {
 	public void FinishQuestions() {
 		nextQuestionBtn.SetActive(false);
 
-		UnityEngine.Debug.Log(fuzzyQuestionary.ToJSON());
+		WWWForm form = new WWWForm ();
+		form.AddField ("json", fuzzyQuestionary.ToJSON());
+
+		WWW www = new WWW("http://localhost:8081/ReceiveFuzzyAnswer/main", form);
+
+		UnityEngine.Debug.Log("FIM");
 	}
 
 	public void Update() {
@@ -116,6 +136,8 @@ public class FuzzyGenerator : MonoBehaviour {
 
 			created = true;
 		}
+
+		ShowQuestion();
 	}
 
 }
